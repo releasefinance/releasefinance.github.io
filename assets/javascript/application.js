6ba2c9564a9d66828742532c690e499fdcb9f4e8
Stef Lewandowski <stef@stef.io>
2015-06-12 16:05:24 +0100

    fire event

--- assets/javascript/application.js
+++ assets/javascript/application.js
@@ -3,13 +3,13 @@ var rangeAdjust = function(value) {
   var price           = $("#fixedprice").html().replace(/\u00A3/g, '');
   var discountedPrice = Math.floor((20 - shares) * (price / 20.0 ))
   // return(Math.floor((20 - this.shares) * (this.car.price / 20.0 )));
+  analytics.track('evoque shares', {shares: shares, discounted_price: discountedPrice});
 
   $('#total').html(shares);
   $('#discountedPrice').html('£' + discountedPrice);
   $('._total').html(shares);
   $('._discountedPrice').html('£' + discountedPrice);
 
-  analytics.track('evoque shares', {shares: shares, discounted_price: discountedPrice});
 
 }
 
