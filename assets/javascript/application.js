71b0811d4c13cbde448f7a1c5fe2a33efe5c7dd5
jasecoop <jase@jasecoop.com>
2015-06-12 11:05:02 +0100

    Evoque page

---
+++ assets/javascript/application.js
@@ -0,0 +1,13 @@
+var rangeAdjust = function(value) {
+  var shares          = value;
+  var price           = $("#fixedprice").html().replace(/\u00A3/g, '');
+  var discountedPrice = Math.floor((20 - shares) * (price / 20.0 ))
+  // return(Math.floor((20 - this.shares) * (this.car.price / 20.0 )));
+
+  $('#total').html(shares);
+  $('#discountedPrice').html('£' + discountedPrice);
+}
+
+$(document).ready(function() {
+  rangeAdjust(0);
+});
