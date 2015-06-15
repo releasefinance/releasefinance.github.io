f8f592634e617e78e0b277c0450d2a0aa48353e5
jasecoop <jase@jasecoop.com>
2015-06-15 11:24:38 +0100

    Shares now int

--- assets/javascript/application.js
+++ assets/javascript/application.js
@@ -2,7 +2,7 @@ var rangeAdjust = function(value) {
   if($("#fixedprice").length == 0) {
     return;
   }
-  var shares          = value;
+  var shares          = parseInt(value);
   var price           = $("#fixedprice").html().replace(/\u00A3/g, '');
   var discountedPrice = Math.floor((20 - shares) * (price / 20.0 ))
   // return(Math.floor((20 - this.shares) * (this.car.price / 20.0 )));
